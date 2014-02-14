f9aaff999d5b17177ec765e9873d1e3454912153
sarawr <mer12014@byui.edu>
2014-02-13 21:52:45 -0700

    L05 Paired Programming Assignment
    fixed a bug

--- Memory/src/memory/Game.java
+++ Memory/src/memory/Game.java
@@ -121,7 +121,7 @@ public class Game {
             System.out.println("Not a valid card selection.");
         }
         else {
-        cardRow = cardSelection/board.rows;
+        cardRow = cardSelection/board.columns;
         cardColumn = (cardSelection - 1)%board.columns;
         
         cardSymbol = nums[cardRow][cardColumn];        
