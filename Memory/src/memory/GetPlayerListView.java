cf5e3ccd075b39bee68c56861127c87e044b9eac
Janis Felsted <fel75001@byui.edu>
2014-02-20 18:09:42 -0700

    06 Paired Assign Added Error Checking

--- Memory/src/memory/GetPlayerListView.java
+++ Memory/src/memory/GetPlayerListView.java
@@ -26,14 +26,43 @@ public class GetPlayerListView {
     
     public String[] getInputNames(){
         int nameIndex;
-        
+        int numPlayers =2; // Change this to a variable if we add more players option
+        boolean finished = false;
+        MemoryError errorMsg = new MemoryError();
         Scanner inFile = new Scanner(System.in);
         
-        for  ( nameIndex=0; nameIndex < numPlayers; nameIndex++){
-            System.out.println("Player #" +(nameIndex+1)+ " please enter your name: ");
+        for  ( nameIndex=0; nameIndex < numPlayers && !finished; nameIndex++){
+            System.out.println("Player #" +(nameIndex+1)+ " please enter your name "
+                    + "(or enter \"Q\" to quit}: ");
             String playersName;
             playersName = inFile.nextLine();
             playersName = playersName.trim();
+            
+            if (playersName.length() < 1){
+              errorMsg.displayError("A name must be at least one character long."
+                      + "Try again."
+                      + "\n\t or enter \"Q\" to quit.");
+              continue;
+            }
+            
+            if ((playersName.equals("Computer"))||(playersName.equals("computer"))) {
+                errorMsg.displayError("This is a reserved name. You can not use it."
+                        + "\"Try again."
+                        + "\n\t or enter \"Q\" to quit.");
+                continue;
+            }
+            
+            if (alreadyUsed(listOfPlayerNames, playersName)){
+                errorMsg.displayError("This name was already entered"
+                        + "\"Try again."
+                        + "\n\t or enter \"Q\" to quit.");
+                continue;
+            }
+            
+            if (playersName.toUpperCase().equals("Q")) { // quit?
+                finished = true;
+                break;
+            } 
             
             //add names to player list of names
             this.listOfPlayerNames[nameIndex]=playersName;
@@ -63,9 +92,18 @@ public class GetPlayerListView {
             System.out.println("\t   " + names[i] + " is Player"+ namePosition);
         }
     }
+        
+        private boolean alreadyUsed(String[] list, String newID) {
+        for (String alreadyInList : list) {
+            if (newID.equals(alreadyInList)) {
+                return true;
+            }           
+        }
+        return false;
+    }
+    }
     
     
 
 
     
-}
