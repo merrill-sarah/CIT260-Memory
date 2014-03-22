b480ad2edf63bae4f52f181e8ed6482a9537efb3
Janis Felsted <fel75001@byui.edu>
2014-03-21 19:31:56 -0600

    W10 J's  Individual Assignment finished

--- Memory/src/citbyui/cit260/sarahjanis/memory/enums/ErrorType.java
+++ Memory/src/citbyui/cit260/sarahjanis/memory/enums/ErrorType.java
@@ -11,5 +11,59 @@ package citbyui.cit260.sarahjanis.memory.enums;
  * @author Janis
  */
 public enum ErrorType {
+    ERROR_101 ("You entered an invalid letter"
+            + "\nPlease enter one of the letters in the menu."),
     
+    ERROR102 ("You entered an invalid letter"
+            + "\nplease enter \"s\" for small, \"m\" for medium, or \"l\" for large board size"),
+    
+    ERROR103 ("Your entry is not valid."
+            + "\nPlease enter a card number or 0 for a hint."),
+    
+    ERROR104("A name must be at least one character long"
+            + "\nPlease enter your name again."),
+    ERROR105("That name was already entered."
+            + "\nPlease enter another name"),
+    
+   
+    
+    ERROR201 ("That card was already selected"
+            + "\nPlease enter another card number or 0 for a hint."),
+    
+    ERROR203 ("That card has already been matched. "
+              + "\nPlease enter another card number or 0 for a hint."),
+    
+    ERROR204 ("Enter a non-blank character for the player's marker."),
+    
+    ERROR205 ("Both players can not use the same character for a marker."),
+    
+    ERROR206 ("There is no active game. You must start a new game before "
+              + "you can take a turn"), 
+    
+    ERROR207 ("The number of rows must be between 3 - 9 and the "
+              + "number of columns must be between 3 - 9 "),
+    
+    ERROR209 ("GameCommands - takeTurn: invalidPlayerTYpe");
+
+    
+    String message;
+    
+    ErrorType(String message) {
+        this.message = message;
+    }
+    
+    public String getMessage() {
+        return this.message;
+    }
+
+    
+    
+    public Object displayError(Object object) {
+        message = (String) object;
+        System.out.println("\t#######################################################################");
+        System.out.println("\t ERROR: " + message);
+        System.out.println("\t#######################################################################\n");
+        return null;
+    }
+       
 }
