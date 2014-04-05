5f7952b400b448e49aaee5ca74c56269c2e4640f
Janis Felsted <fel75001@byui.edu>
2014-04-04 22:13:53 -0600

    GameFrames Created

--- Memory/src/citbyui/cit260/sarahjanis/memory/frames/OptionFrame.java
+++ Memory/src/citbyui/cit260/sarahjanis/memory/frames/OptionFrame.java
@@ -163,8 +163,8 @@ public class OptionFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        GameBoardFrame board = new GameBoardFrame();
-        board.setVisible(true);   
+        BoardLargeFrame boardLg = new BoardLargeFrame();
+        boardLg.setVisible(true);   
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
